0469d3dc1af4b5167339605cbecf34dae7bb22aa
Ana Silva <anasilva@lsd.ufcg.edu.br>
2018-09-20 11:33:19 -0300

    update server.js - add call for static files

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/'));
+app.use(express.static(__dirname + '/dist/frontpage-ui'));
 app.use('/src/assets', express.static(__dirname + '/src/assets/'));
 
 const PORT = process.env.PORT || 4200;
+
+app.get('/*', function(req,res) {
+    res.sendFile(path.join(__dirname+'/dist/frontpage-ui/index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Our app is running on port ${ PORT }`);
