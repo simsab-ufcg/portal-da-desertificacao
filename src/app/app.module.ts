96f88bfde23096035c35f60bce6dfaacab71687e
Ana Silva <anasilva@lsd.ufcg.edu.br>
2018-09-20 14:18:34 -0300

    created news component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,41 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { FooterComponent } from './footer/footer.component';
-import { PartnersComponent } from './partners/partners.component';
-import { AboutComponent } from './about/about.component';
-import { TeamComponent } from './team/team.component';
-import {RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import {AppComponent} from './app.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {FooterComponent} from './footer/footer.component';
+import {PartnersComponent} from './partners/partners.component';
+import {AboutComponent} from './about/about.component';
+import {TeamComponent} from './team/team.component';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {NewsComponent} from './news/news.component';
 
 const router: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'about', component: AboutComponent},
-  {path: 'partners', component: PartnersComponent},
-  {path: 'team', component: TeamComponent}
+    {path: '', component: HomeComponent},
+    {path: 'about', component: AboutComponent},
+    {path: 'partners', component: PartnersComponent},
+    {path: 'team', component: TeamComponent},
+    {path: 'news', component: NewsComponent}
 ];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    FooterComponent,
-    PartnersComponent,
-    AboutComponent,
-    TeamComponent,
-    HomeComponent
-  ],
-  imports: [
-    RouterModule,
-    BrowserModule,
-    RouterModule.forRoot(router),
-  ],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        NavbarComponent,
+        FooterComponent,
+        PartnersComponent,
+        AboutComponent,
+        TeamComponent,
+        HomeComponent,
+        NewsComponent
+    ],
+    imports: [
+        RouterModule,
+        BrowserModule,
+        RouterModule.forRoot(router),
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
