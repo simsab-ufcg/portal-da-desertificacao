6de1ac22a4cfeb149c16c52094ef9a8ae84ed6a5
Ana Silva <anasilva@lsd.ufcg.edu.br>
2018-09-20 11:10:00 -0300

    simplify server.js

--- server.js
+++ server.js
@@ -1,18 +1,9 @@
-//Install express server
 const express = require('express');
-
 const path = require('path');
-
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/'));
+app.use('/src/assets', express.static(__dirname + '/src/assets/'));
 
-app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname+'/dist/index.html'));
-});
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+app.listen(process.env.PORT || 8080);
