9f3d91464dc32027b4b578d0d721f2e89122b4fb
Ana Silva <anasilva@lsd.ufcg.edu.br>
2018-09-20 11:26:46 -0300

    update server.js and package.json

--- server.js
+++ server.js
@@ -5,5 +5,8 @@ const app = express();
 app.use(express.static(__dirname + '/dist/'));
 app.use('/src/assets', express.static(__dirname + '/src/assets/'));
 
+const PORT = process.env.PORT || 4200;
 
-app.listen(process.env.PORT || 8080);
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
