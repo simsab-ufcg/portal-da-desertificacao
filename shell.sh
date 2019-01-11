6f0ac3a2f7f406aa9500753129516aa3aea8cd29
pedro.pacheco <pedro.pacheco@insa.gov.br>
2019-01-11 14:06:42 -0300

    commit-8

--- shell.sh
+++ shell.sh
@@ -5,11 +5,11 @@ docker_prod=$(docker ps -aqf "name=swarm-prod.insa.gov.br")
 
 if [ "$docker" == "$docker" ]; then
    docker rm -f $docker > /dev/null 2>&1 &
-   echo "Apagando $docker.."
+   echo "Apagando Homologação: $docker"
 
   if [ "$docker_prod" == "$docker_prod" ];then
   docker rm -f $docker_prod > /dev/null 2>&1 &
-  echo "Apagando $docker_prod"
+  echo "Apagando Produção: $docker_prod"
 
   else 
     exit 0 
